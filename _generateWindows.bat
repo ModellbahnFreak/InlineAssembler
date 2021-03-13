4a021568e68e4cc4692d5394f03c0785d8a9c892
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-03-13 18:07:25 +0100

    Added search for vs cmake

--- _generateWindows.bat
+++ _generateWindows.bat
@@ -2,11 +2,19 @@
 
 where /q cmake
 IF ERRORLEVEL 1 (
-    echo "CMake wasn't found. This means it's propably not installed."
-    echo "Please download and install CMake"
-    echo "IMPORTAND: Make sure, you set CMake to be included in PATH"
-    pause
-    start https://github.com/Kitware/CMake/releases/latest
+    "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake" --help >nul 2>&1
+    IF %errorlevel% != 0 (
+        "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake" --help >nul 2>&1
+        IF %errorlevel% != 0 (
+            echo "This application requires an installed Visual Studio and CMake."
+            echo "CMake wasn't found. This means it's propably not installed."
+            echo "Please download and install CMake"
+            echo "IMPORTAND: Make sure, you set CMake to be included in PATH"
+            pause
+            start https://github.com/Kitware/CMake/releases/latest
+            exit 1
+        )
+    )
 )
 
 cmake -A Win32 .
