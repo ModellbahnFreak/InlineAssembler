3e734d75d5c4dfe0d2ffebfdbc5559e1b0aa0ff9
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-03-13 15:13:44 +0100

    Added correct header file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include "InlineAssemblerConfig.h"
 
+long foo(int A) {
+    int out;
+    // You can't assume A will be in RDI: after inlining it prob. won't be
+    __asm {
+        call test
+        mov   ecx, A                   // comment syntax
+        add   ecx, 5
+        mov   out, ecx
+    }
+    ret
+
 int main(int argc, char** argv) {
     std::cout << "Code for sheet " << SHEET_NUM << " task " << TASK_NUM << std::endl;
+    std::cout << "Result of foo(12): " << foo(12) << std::endl;
 }
