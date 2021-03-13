9e7042ccaf2d37686e86eb5609624fad66820a11
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-03-13 17:36:34 +0100

    Added windows generate script

---
+++ _generateWindows.bat
@@ -0,0 +1,11 @@
+where /q cmake
+IF ERRORLEVEL 1 (
+    echo "CMake wasn't found. This means it's propably not installed."
+    echo "Please download and install CMake"
+    echo "IMPORTAND: Make sure, you set CMake to be included in PATH"
+    pause
+    start https://github.com/Kitware/CMake/releases/latest
+)
+
+cmake -A Win32 .
+start .\InlineAssembler.sln
