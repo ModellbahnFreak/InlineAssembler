519f6e2333f32fabd61e9c1d89e5183a80ae5888
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-03-13 18:47:52 +0100

    Fixed errorlevel bug in windows generation script

--- _generateWindows.bat
+++ _generateWindows.bat
@@ -3,17 +3,17 @@
 where /q cmake
 IF ERRORLEVEL 1 (
     "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake" --help >nul 2>&1
-    IF %errorlevel% neq 0 (
+    IF ERRORLEVEL 1 (
         "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake" --help >nul 2>&1
-        IF %errorlevel% neq 0 (
+        IF ERRORLEVEL 1 (
             "C:\Program Files (x86)\Microsoft Visual Studio\2019\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake" --help >nul 2>&1
-            IF %errorlevel% neq 0 (
+            IF ERRORLEVEL 1 (
                 "C:\Program Files\Microsoft Visual Studio\2019\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake" --help >nul 2>&1
-                IF %errorlevel% neq 0 (
-                    echo "This application requires an installed Visual Studio and CMake."
-                    echo "CMake wasn't found. This means it's propably not installed."
-                    echo "Please download and install CMake"
-                    echo "IMPORTAND: Make sure, you set CMake to be included in PATH"
+                IF ERRORLEVEL 1 (
+                    echo This application requires an installed Visual Studio and CMake.
+                    echo CMake wasn't found. This means it's propably not installed.
+                    echo Please download and install CMake
+                    echo IMPORTAND: Make sure, you set CMake to be included in PATH
                     pause
                     start https://github.com/Kitware/CMake/releases/latest
                     exit 1
