1cb4066428f6b3f87e754a9545953c081ad56e98
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-03-13 15:02:34 +0100

    Added test project

---
+++ InlineAssemblerConfig.h
@@ -0,0 +1,2 @@
+#define SHEET_NUM 1
+#define TASK_NUM 2
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "InlineAssemblerConfig.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Code for sheet " << SHEET_NUM << " task " << TASK_NUM << std::endl;
+}
